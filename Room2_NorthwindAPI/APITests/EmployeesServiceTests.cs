465376e8bbd3bdba251db0044e4858f223df19f2
unloosedcascade <matthewdan.handley@btinternet.com>
2023-05-04 16:53:51 +0100

    added usings

--- Room2_NorthwindAPI/APITests/EmployeesServiceTests.cs
+++ Room2_NorthwindAPI/APITests/EmployeesServiceTests.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Room2_NorthwindAPI.Models;
+using Room2_NorthwindAPI.Data.Repositories;
+using Room2_NorthdwindAPI.Controllers;
+using Room2_NorthwindAPI.Services;
 
 namespace APITests;
 
 internal class EmployeesServiceTests
 {
+
+    private EmployeesRepository _repository
 }
